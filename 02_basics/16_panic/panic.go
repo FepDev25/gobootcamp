2a198df91134f8bda0d62b027027227abb7c69a3
Felipe <lamediatricolor@gmail.com>
2025-09-13 21:30:18 -0500

    Panic

--- 02_basics/16_panic/panic.go
+++ 02_basics/16_panic/panic.go
@@ -3,6 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Start")
+	// In go, a panic is a built-in function that stops the normal execution of a program.
+	// It stops the execution of the current function and begins panicking, and then executes any deferred functions.
+	//process(10)
+	process(-5) // This will cause a panic
+	fmt.Println("This line will not be executed due to panic")
+}
 
+func process(input int) {
+	defer fmt.Println("Defer 1")
+	defer fmt.Println("Defer 2")
+
+	if input < 0 {
+		panic("Negative input not allowed")
+	}
+	fmt.Println("Processing:", input)
 }
